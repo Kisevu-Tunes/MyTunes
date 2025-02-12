87cb65a63ed6a0dc72215663a1ff32af5f81614d
ameda-kevin <amedakevin@gmail.com>
2025-02-12 10:29:31 +0300

    retrieval and manipulation of user data via the user service successfully working

--- user-service/src/main/java/com/ameda/works/user_service/controller/UserApi.java
+++ user-service/src/main/java/com/ameda/works/user_service/controller/UserApi.java
@@ -1,13 +1,11 @@
 package com.ameda.works.user_service.controller;
 
-import com.ameda.works.user_service.model.User;
+import com.ameda.works.user_service.dto.UserDTO;
+import com.ameda.works.user_service.mapper.DTOMapper;
 import com.ameda.works.user_service.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -15,14 +13,34 @@ public class UserApi {
 
 
     private final UserService userService;
+    private final DTOMapper dtoMapper;
 
-    public UserApi(UserService userService) {
+    public UserApi(UserService userService, DTOMapper dtoMapper) {
         this.userService = userService;
+        this.dtoMapper = dtoMapper;
     }
 
     @PostMapping("/create")
-    public ResponseEntity<?> saveUser(@RequestBody User user){
-        userService.save(user);
+    public ResponseEntity<UserDTO> saveUser(@RequestBody UserDTO user){
+        userService.save(dtoMapper.map(user));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable String id){
+        return ResponseEntity.ok(dtoMapper.map(userService.getById(id)));
+    }
+
+    @DeleteMapping("/user/{id}")
+    @ResponseBody
+    public void deleteUserById(@PathVariable String id){
+        userService.deleteUser(id);
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable String id,
+                                        @RequestBody  UserDTO userDTO){
+        userService.update(dtoMapper.map(userDTO),id);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- user-service/src/main/java/com/ameda/works/user_service/mapper/DTOMapper.java
+++ user-service/src/main/java/com/ameda/works/user_service/mapper/DTOMapper.java
@@ -1,6 +1,13 @@
 package com.ameda.works.user_service.mapper;
 
+import com.ameda.works.user_service.dto.UserDTO;
+import com.ameda.works.user_service.model.User;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public abstract class DTOMapper {
 
+
+    public abstract User map(UserDTO userDTO);
+    public abstract UserDTO map(User user);
 }
